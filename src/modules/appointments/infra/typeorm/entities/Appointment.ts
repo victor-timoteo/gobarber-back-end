d41af420ed80a8abc335505208b7506536dc3d99
Victortimoteo <vitortimoteo@hotmail.com>
2020-08-17 17:38:51 -0300

    backend

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -29,7 +29,7 @@ class Appointment {
   @JoinColumn({ name: 'user_id' })
   user: User;
 
-  @Column('time with time zone')
+  @Column('timestamp with time zone')
   date: Date;
 
   @CreateDateColumn()
